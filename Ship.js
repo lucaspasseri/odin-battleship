b3266e50436d59342e3e99d1995223f757fa7b1d
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2025-09-29 15:17:45 -0300

    Create Ship class and its tests

--- Ship.js
+++ Ship.js
@@ -1,1 +1,33 @@
-export default class Ship {}
+export default class Ship {
+	#length = 0;
+	#hitNumber = 0;
+	constructor(length) {
+		if (
+			typeof length !== "number" ||
+			Number.isNaN(length) ||
+			length < 1 ||
+			length > 10
+		) {
+			throw new Error();
+		}
+		this.#length = length;
+	}
+
+	get length() {
+		return this.#length;
+	}
+
+	get hitNumber() {
+		return this.#hitNumber;
+	}
+
+	isSunk() {
+		return this.#hitNumber >= this.#length;
+	}
+
+	hit() {
+		if (!this.isSunk()) {
+			this.#hitNumber += 1;
+		}
+	}
+}
--- Ship.test.js
+++ Ship.test.js
@@ -1,5 +1,29 @@
 import Ship from "./Ship";
 
-test("Ship Class is imported correctly", () => {
-	expect(() => new Ship()).not.toThrow();
+test("throws error for invalid lengths", () => {
+	const invalidValues = [undefined, null, 0, -1, 11, "2", NaN, {}, []];
+	invalidValues.forEach(len => expect(() => new Ship(len)).toThrow());
+});
+
+test("hit() increases hit count when ship is not sunk", () => {
+	const ship = new Ship(2);
+	ship.hit();
+	expect(ship.hitNumber).toBe(1);
+});
+
+test("hit() does not increase hit count once ship is sunk", () => {
+	const ship = new Ship(1);
+	ship.hit();
+	expect(ship.hitNumber).toBe(1);
+	ship.hit();
+	expect(ship.hitNumber).toBe(1);
+});
+
+test("isSunk() reflects the sunk state", () => {
+	const ship = new Ship(2);
+	expect(ship.isSunk()).toBe(false);
+	ship.hit();
+	expect(ship.isSunk()).toBe(false);
+	ship.hit();
+	expect(ship.isSunk()).toBe(true);
 });
